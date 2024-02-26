916a974d259e085fc53a1879907cd5981afda31f
andrecampll <andrevictor50@gmail.com>
2024-02-26 01:08:22 -0300

    feat: adds responsive grid and drawer component

---
+++ src/components/add-button.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { Drawer as Vaul } from 'vaul'
+
+export function AddButton() {
+  return (
+    <Vaul.Root shouldScaleBackground>
+      <Vaul.Trigger asChild>
+        <div className="fixed bottom-0 right-0 mb-4 mr-4 rounded-[50%] shadow-md lg:hidden">
+          <button
+            onClick={() => {}}
+            className="flex h-[40px] w-[40px] items-center justify-center rounded-[50%] bg-gradient-to-r from-primary to-secondary p-2"
+          >
+            <span className="text-3xl font-bold text-white">+</span>
+          </button>
+        </div>
+      </Vaul.Trigger>
+      <Vaul.Portal>
+        <Vaul.Overlay className="fixed inset-0 bg-black/40" />
+        <Vaul.Content className="fixed bottom-0 left-0 right-0 mt-24 flex h-[96%] flex-col rounded-t-[10px] bg-zinc-100">
+          <div className="flex-1 rounded-t-[10px] bg-white p-4">
+            <div className="mx-auto mb-8 h-1.5 w-12 flex-shrink-0 rounded-full bg-zinc-300" />
+            test
+          </div>
+        </Vaul.Content>
+      </Vaul.Portal>
+    </Vaul.Root>
+  )
+}
